cd8c310455173b19341ba5966f94aeebbc1c78a8
w0nnie <87374274+w0nnie@users.noreply.github.com>
2025-02-02 17:42:09 +0900

    Feature/#1 프로젝트 초기설정 (#4)
    * init

---
+++ src/test/java/com/pododoserver/PododoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pododoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PododoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
