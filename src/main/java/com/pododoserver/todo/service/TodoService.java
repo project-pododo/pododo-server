ed0c05d71ed9c7c788404f5d968098f07b6d8c3f
zinnie0e <zinnie0e@naver.com>
2025-02-05 22:47:57 +0900

    [chore] - 패키지 구조 설계
    [feat]
    - test API 구현

---
+++ src/main/java/com/pododoserver/todo/service/TodoService.java
@@ -0,0 +1,12 @@
+package com.pododoserver.todo.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+//@Transactional
+@RequiredArgsConstructor
+public class TodoService {
+
+    // todo 비지니스 로직
+}
